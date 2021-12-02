bb830eeaed29ac87b71722277ccd450a87e39e69
Oleg-Maruschak <maruschak87@gmail.com>
2021-12-02 13:44:23 +0200

    Update screen_url.py
    Добавлена проверка отсутствия вначале строки http/s

--- screen_url.py
+++ screen_url.py
@@ -12,13 +12,21 @@ class Screen:
         self.driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=self.options)
         self.driver.implicitly_wait(5)
         self.returnAnsver = {'err': False}
+        format_url = self.format_url(url)
         try:
-            self.driver.get(url)
+            self.driver.get(format_url)
             self.driver.save_screenshot(f'./{id_user}.png')
             self.returnAnsver['screen'] = f'{id_user}.png'
         except Exception:
             self.returnAnsver['err'] = True
         self.driver.quit()
+
+    def format_url(self, url):
+        find_http = url.lower().find('http')
+        if find_http == 0:
+            return url
+        else:
+            return f'https://{url}'
 
     def return_result(self):
         return self.returnAnsver
